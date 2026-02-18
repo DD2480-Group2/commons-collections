3a85b0eda2689cc266261a266322795eb4928226
Teoman Köylüoglu <151879574+Laafayeette@users.noreply.github.com>
2026-02-18 15:59:15 +0100

    feat: Add testput10 4/4 (Closes #10)
    Extends branch coverage for case 1 in the drop-through when updating existing map where key != null.

--- src/test/java/org/apache/commons/collections4/map/Flat3MapTest.java
+++ src/test/java/org/apache/commons/collections4/map/Flat3MapTest.java
@@ -629,6 +629,83 @@ public class Flat3MapTest<K, V> extends AbstractIterableMapTest<K, V> {
         assertEquals(4, m.size());
     }
 
+    /**
+     * Tests the condition when a hash collision occurs between two distinct strings.
+     * The if-check enforces boolean && condition, thus we expect a new mapping to occur.
+     *
+     */
+    @Test
+    void testPut10() {
+        final Flat3Map<Object, Integer> m = new Flat3Map<>();
+
+        m.put("FB", ONE);
+        m.put(TWO, TWO);
+        m.put(THREE, THREE);
+
+        assertEquals("FB".hashCode(), "Ea".hashCode()); // Known collision
+        assertNotEquals("FB", "EA");
+
+        final Object old = m.put("Ea", Integer.valueOf(4)); // Triggers DelegateMap
+
+        assertNull(old); // Since not existing update
+        assertEquals(4, m.get("Ea"));
+        assertEquals(ONE, m.get("FB"));
+        assertEquals(4, m.size());
+    }
+
+    /**
+     * Tests the condition when a hash collision occurs between two distinct strings.
+     * The if-check enforces boolean && condition, thus we expect a new mapping to occur.
+     *
+     */
+    @Test
+    void testPut11() {
+        final Flat3Map<Object, Integer> m = new Flat3Map<>();
+
+        m.put(ONE, ONE);
+
+        final Object old = m.put(ONE, TWO);
+        assertEquals(TWO, m.get(ONE));
+        assertEquals(ONE, old);
+    }
+
+    /**
+     * Tests the condition when a hash collision occurs between two distinct strings.
+     * The if-check enforces boolean && condition, thus we expect a new mapping to occur.
+     *
+     */
+    @Test
+    void testPut12() {
+        final Flat3Map<Object, Integer> m = new Flat3Map<>();
+
+        m.put(ONE, ONE);
+        m.put(TWO, TWO);
+
+        final Object old = m.put(ONE, TWO);
+        assertEquals(TWO, m.get(ONE));
+        assertEquals(ONE, old);
+    }
+
+    /**
+     * Tests the condition when a hash collision occurs between two distinct strings.
+     * The if-check enforces boolean && condition, thus we expect a new mapping to occur.
+     *
+     */
+    @Test
+    void testPut13() {
+        final Flat3Map<Object, Integer> m = new Flat3Map<>();
+
+        m.put(ONE, ONE);
+        m.put(TWO, TWO);
+        m.put(THREE, THREE);
+
+        final Object old = m.put(ONE, TWO);
+        assertEquals(TWO, m.get(ONE));
+        assertEquals(ONE, old);
+    }
+
+
+
     @Test
     void testRemove1() {
         final Flat3Map<Integer, Integer> m = new Flat3Map<>();
